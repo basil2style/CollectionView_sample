f92ee1731a8b4f61ff31af30f255ea4aec7c436c
Basil <basiltalias92@gmail.com>
2017-03-29 20:08:39 -0400

    Initial commit

---
+++ CollectionView/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  CollectionView
+//
+//  Created by Basil on 2017-03-29.
+//  Copyright © 2017 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -8,16 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource  {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    var images = ["bg","flappy1","flappy2","pipe1","pipe2"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as! CollectionViewCell
+        cell.imageView.image = UIImage(named: images[indexPath.row])
+        return cell
     }
 
 
